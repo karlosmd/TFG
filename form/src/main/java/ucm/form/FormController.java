e31b22eaf9fc9beee5a3c1475ce87fe6b418a00d
Ivan Martinez-Ortiz <imartinezortiz@gmail.com>
2017-11-16 18:22:30 +0100

    Renombrando Greeting a Usuario

--- form/src/main/java/ucm/form/FormController.java
+++ form/src/main/java/ucm/form/FormController.java
@@ -1,7 +1,10 @@
 package ucm.form;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,12 +13,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 public class FormController {
 	@GetMapping("/greeting")
     public String greetingForm(Model model) {
-        model.addAttribute("greeting", new Greeting());
+        model.addAttribute("greeting", new Usuario());
         return "greeting";
     }
 
     @PostMapping("/greeting")
-    public String greetingSubmit(@ModelAttribute Greeting greeting) {
+    public String greetingSubmit(@Valid @ModelAttribute Usuario greeting, BindingResult errores) {
         return "result";
     }
 }
